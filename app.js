f2e2faea7639763efc3cee132e746b98b3c28d16
mtd6458 <md.yu.03@gmail.com>
2019-06-16 16:18:57 +0900

    Update noticeGarbageDay

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 function noticeGarbageDay() {
-  var accessToken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'; 
-
-  var date = new Date();
-  date.setDate(date.getDate() + 1);
-  var comment = [];
-  
-  var is_the_what_weekly = Math.floor((date.getDate() - 1) / 7) + 1;
+  var accessToken     = 'xxxxxxxxxxxxxxx'; 
+  var comment         = [];
+  var date            = new Date();
+  var weeklyNumber    = Math.floor((date.getDate() - 1) / 7) + 1;
 
   switch(date.getDay()) {
-      case 1:
-      comment.push('一般ごみ')
-      break;
-      case 2:
-      comment.push('ダンボール・リサイクルごみ')
-      break;
-      case 3:
-      comment.push('プラスチックごみ')
-      break;
-      case 4:
-      comment.push('一般ごみ')
-      break;
-      case 5:
-      comment.push('びん・缶ごみ')
-      if (is_the_what_weekly === 2 || is_the_what_weekly === 4) {
-          comment.push('小型複雑ごみ')
+    case 0: // 日曜日
+      comment.push('燃えるごみ')
+      break;      
+    case 1: // 月曜日
+      if (weeklyNumber === 2) {
+        comment.push('月1回の空きびん・ペットボトル')
+      }
+      if (weeklyNumber === 4) {
+        comment.push('月1回の燃えないごみ\n ・空き缶\n ・ガラス類\n ・金属類(鍋、やかん、フライパン、針金製ハンガー)\n ・傘\n ・蛍光管\n ・小型家電類(オーブントースター、炊飯器とかとか)\n')
       }
       break;
+    case 3: // 水曜日
+      comment.push('一般ごみ')
+      break;
+    case 2: // 火曜日
+    case 4: // 木曜日
+    case 5: // 金曜日
+    case 6: // 土曜日
+      break;
+  }
+   
+  if (comment.length < 1) {
+    return
   }
   
-  if (comment.length < 1) return;
-  
-  var text = '\n明日は' + comment.join('、') + ' の日ですからね!\n忘れちゃだめですよご主人様!!' ;
-  
-  var message = text;
+  var message = '\n今日は' + comment.join('、') + 'を捨てる日だよ。\nえっへん！' ;
   var options = {
     'method' : 'post',
     'headers': {
